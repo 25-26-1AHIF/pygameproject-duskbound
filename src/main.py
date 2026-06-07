a856b5260f142e8e30bb5d208aebf4cc99f042de
Recep Öztürk <recep.oeztuerk@student.htl-rankweil.at>
2026-06-07 16:28:57 +0200

    Einstiegspunkt Main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
-print("Testen1")
+import pygame
+from game import Game
+from settings import WINDOW_WIDTH, WINDOW_HEIGHT
 
-print("was geht")
+def main() -> None:
+    pygame.init()
+    pygame.display.set_caption("Duskbound - Demo")
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    Game(screen).run()
+    pygame.quit()
 
-print("hello")
-hello
+if __name__ == "__main__":
+    main()
